added cycle basis computation; make barcode legend unique; fixed minor problem when max_non_infinity=0

main.py
@@ -4,6 +4,7 @@ from blowup_complex import ProductSimplex, BlowupComplex
 from complex import DelaunayComplex
 from simplicial_cover import HyperCube
 import numpy as np
+from matplotlib import pyplot as plt
 
 import util
 
@@ -28,9 +29,19 @@ if __name__=='__main__':
     blowup = BlowupComplex([X0,X1])
     blowup.compute_persistence()
     """
+    
+    X0= [[0],[1],[2],[0,1], [1,2]]  
+    X1=[[1],[2],[3] ,[1,2],[2,3]]  
+    X2 = [[0],[3],[0,3]]
+    X3=[]
+    blowup = BlowupComplex([X0,X1,X2,X3])
+    
+    blowup.compute_persistence(verbose=False)
+    print('all basis',blowup.cycle_basis)
 
     
-
+    
+    
     """ 
     a cycle with three covers 
     ```
@@ -43,31 +54,49 @@ if __name__=='__main__':
      ```
     The covers are glued by a 2D standard simplex $\Delta^2$ 
     """
-    """ 
+    
     X0= [[0],[1],[2],[3],[0,1],[1,2],[2,3]]  
     X1=[[2],[3],[4],[5] ,[2,3],[3,4],[4,5]]   
     X2= [[0],[1],[4],[5],[0,1],[0,5],[4,5]]
     blowup = BlowupComplex([X0,X1,X2])
-    blowup.compute_persistence()
-    """ 
+    blowup.compute_persistence(verbose=False )
+    print('all basis',blowup.cycle_basis)
+    
 
     """
     point cloud with hypercube cover
     """
     circles_data = util.threecircles(N=20,s=42)
     delaunay_complex = DelaunayComplex(np.array(circles_data ),4)#生成2Dmesh
-    #print("edge list:", test_complex.edge_index()) #边
-
-    delaunay_complex.draw_complex() #绘制
-    #plt.scatter(circles_data[:,0],circles_data[:,1],color = "red")
-    #plt.show() 
-
+    
+    N=3 # columns
+    M=3 # rows
     a = HyperCube(circles_data,delaunay_complex.edge_index(), 
-                N=6,  M=4,  r=0.8 )
-    print("HyperCube cover size:", len(a.cover()))
-    a.draw_cover()
-    #return a list,with N*M sublists represnted N*M different covers(only vertices and edges)
+                N,  M,  r=1  )
+    print("Create hyperCube cover of size:", len(a.cover))
+    #a.draw_cover()
+
+    # Create the figure and axes objects
+    fig, axs = plt.subplots(M, N, figsize=(N * 3, M * 3))
+
+    # Loop over the rows and columns
+    for i in range(M): #row
+        for j in range(N): #col
+            # Determine the current axes
+            if M > 1:
+                ax = axs[ i,j]
+            else:
+                ax = axs[j] 
+            a.draw_cover_subset(i,j,ax)
+
+
+    plt.tight_layout()
+    plt.show()
+    
+    
+    #return a list,with N*M sublists represnted N*M different covers
+    # (only vertices and edges)
 
     # to compute the localized homology, directly use the cover as the input of BlowupComplex()
-    blowup = BlowupComplex(a.cover())
-    blowup.compute_persistence(show_diag=False)
+    blowup = BlowupComplex(a.cover)
+    dgms = blowup.compute_persistence(show_diag=False)

persistence.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import cechmate as cm  
 import phat
 import itertools
+import util
 
 def _process_distances(pairs, ordered_simplices,show_diag=False,cover=None):
     """ Setup persistence diagrams by reading off distances
@@ -50,7 +51,7 @@ def _add_unpaired(dgms, pairs, simplices,cover=None):
 
     for i in range(len(posneg)):
         if posneg[i] == 0:
-            (idxs, dist) = simplices[i]
+            (idxs, dist) = simplices[i] # index is the simplex, # dist is the filtration parameter
             p = len(idxs) - 1
             if not p in dgms:
                 dgms[p] = []
@@ -58,15 +59,15 @@ def _add_unpaired(dgms, pairs, simplices,cover=None):
                 dgms[p].append([dist, np.inf,cover[i]])
             else:
                 dgms[p].append([dist, np.inf])
+            pairs.append_pair(  i,  -1 )  # add infinity as "-1"
+    return dgms,pairs
 
-    return dgms
 
-
-def max_non_infinite(numbers):
+def _max_non_infinite(numbers):
     # Filter out infinite values
     finite_numbers = [num for num in numbers if num != float('inf')  ]
     if not finite_numbers:
-        return 10
+        return 10 
     return max(finite_numbers)
     
 def plotbarcode_BC(dgms,dim,coverlabel2id):
@@ -75,20 +76,22 @@ def plotbarcode_BC(dgms,dim,coverlabel2id):
     
     _, axes = plt.subplots(1, 1,figsize=(4,1.5))
     colormap = plt.cm.tab20.colors
-    print(persistence)
-    
+
     death = [d for b,d,c in dgms[dim]]
     
-    largest_non_infinite = max_non_infinite( death )
-    
-    infinity  = largest_non_infinite*1.5
+    largest_non_infinite = _max_non_infinite( death )
+    # if max non infinite is zero, 
+    if largest_non_infinite == 0:
+        infinity = 10
+    else:
+        infinity  = largest_non_infinite*1.8
     
     x = [birth for (dim, (birth, death)) in persistence]
     y = [(death - birth) if death != float("inf") else (infinity - birth) for (dim, (birth, death)) in persistence]
     c = [colormap[dim % len(colormap) ] for (dim, (birth, death)) in persistence]
     
     axes.barh(range(len(x)), y, left=x, alpha=0.5, color=c, linewidth=0,label=labels)
-    plt.axvline(x=infinity , color='red', linestyle='--') 
+    axes.axvline(x=infinity , color='red', linestyle='--') 
 
 
     # Shrink current axis by 20%
@@ -96,14 +99,14 @@ def plotbarcode_BC(dgms,dim,coverlabel2id):
     axes.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 
     # Put a legend to the right of the current axis
-    axes.legend(loc='center left', bbox_to_anchor=(1, 0.5),ncol = 3)#oc="lower right")
-
+    util.legend_without_duplicate_labels(axes)
+    
     #axes.legend( )
-    axes.set_title("Persistence barcode", fontsize=12)
+    axes.set_title("Persistence barcode (H%d)" % dim, fontsize=12)
     axes.set_yticks([])   #axes.invert_yaxis()
     plt.show()
 
-def compute_persistence(ordered_simplices, columns,cover ,show_diag=False):
+def compute_persistence(ordered_simplices, columns,cover ,show_diag=False,verbose=False):
     if show_diag:
         print('display homology classes on the diagonal')
     
@@ -117,15 +120,47 @@ def compute_persistence(ordered_simplices, columns,cover ,show_diag=False):
     
     # compute persistence pairs (hide diagonals to make it easier to read)
     dgms =  _process_distances(pairs, ordered_simplices,show_diag,cover)
-    for p in pairs:
-        print(p)
-    print(dgms)
-    dgms =  _add_unpaired(dgms, pairs, ordered_simplices,cover)
+    dgms ,pairs =  _add_unpaired(dgms, pairs, ordered_simplices,cover)
     
+    cycle_basis = dict()
+    for p in pairs:
+        if verbose:
+            print(p)
+        if p[1]==-1:
+            cycle_basis[p[0]] =  find_basis(p[0],columns,pairs) 
+        
     # assign a color id to each subset combination 
     coverlabel2id = dict([ (c,i) for i,c in enumerate(set(cover)) ])
     for dim,dgm in dgms.items(): 
-        print('H%d'%dim,dgm )
+        print('H%d |'%dim,dgm )
         plotbarcode_BC(dgms,dim,coverlabel2id )  
-    return dgms
+    
+    return dgms, cycle_basis 
+
+def project_chain_to_x(ordered_simplices,chain, cover):
+    # given a chain (list of integer) indexed in the product complex
+    # return a representation [( simplex, 'coverlabel')]
+    return list(map(lambda s: (ordered_simplices[s][0],cover[s] ),chain )) 
+    
+
+def find_basis(sid,columns,pairs ):
+    # sid is the id of a creator. we repeatedly find the cascade of sid, 
+    # which forms the basis of the homology class
+    cascade = set([sid])
+    basis = set([sid])
+    partner = [0]* len(columns) 
+    for birth,death in pairs:
+        partner[birth] = death
+        partner[death] = birth 
+    while cascade :
+        sigma = cascade.pop()
+        dim, boundary = columns[sigma]
+        for tau in boundary:
+            
+            if partner[tau] != -1 and partner[tau] not in basis:
+                cascade.add(partner[tau]) 
+        basis.add(sigma)
+    return basis
+
+
     